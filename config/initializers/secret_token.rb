9c8ffe82bdee764f1cfbda8c3b34cc87027b1053
helbez <helbez@hot.ee>
2012-09-05 19:54:59 +0300

    setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Helbefoto::Application.config.secret_token = '3f2380dabd85fb0edeae809346abdb5dd0518ad0073204457d3ff0d65af037e0c8ecb3464dbbe29c44c7d8869ad747c56866b4cf5ce8f7a7593ec53daa9c86ab'
